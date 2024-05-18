6f6775102ccf31515077465f3f6d84c76aaa0e38
smontalbano <sdm030@gmail.com>
2024-05-18 16:26:25 -0400

    Added GUI elements and fixed rinbow color code.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const container = document.querySelector(".container");
+const radioButtons = document.querySelector(".radioButtons");
+const changeSize = document.querySelector(".grid-button");
 let size = 16;
-
 buildBoard(size);
 
+radioButtons.addEventListener("change", () => {
+    removeBoard();
+    buildBoard(size);
+})
+
+changeSize.addEventListener("click", () => {
+    size = prompt("Enter size of grid:", size);
+    removeBoard();
+    buildBoard(size);
+})
+
 function buildBoard(size) {
+    let colorMode = document.querySelector("#rainbow").checked;
     gridSize = size * size;
     for (i = 0; i < gridSize; i++) {
         const boxWidth = (1 / size) * 100;
@@ -14,10 +27,26 @@ function buildBoard(size) {
         box.style.height = boxHeight + "%";
         box.style.border = "1px solid black";
         container.appendChild(box);
-
         box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "black";
-        }); 
-  }
+            if (colorMode === true){
+                let color = getColor();
+                box.style.backgroundColor = color;
+            } else {
+                box.style.backgroundColor = "black";
+            }
+        });
+    };
+}
+
+function removeBoard(){
+    const content = document.getElementsByClassName("box");
+    while (content.length > 0) {
+        content[0].remove()
+    }
+}
+
+function getColor(){
+    let color = Math.floor(Math.random() * 16777216).toString(16);
+    return '#000000'.slice(0, -color.length) + color;
 }
 
